f5a6dcd9a7efd91fc0a0be6ed7afeaaa8f5bdb94
migdal <mcjmigdal@gmail.com>
2021-08-10 00:26:27 +0200

    increase pictures quality to 20

--- bin/takepic.sh
+++ bin/takepic.sh
@@ -3,4 +3,4 @@
 # Take picture of the grow box
 outdir="${HOME}/shiitake/data/img"
 DATE=$(date +"%Y-%m-%d_%H%M")
-raspistill -v -n -t 1 -a 12 -ae 64 -th none -q 10 -o ${outdir}/${DATE}.jpg
+raspistill -v -n -t 1 -a 12 -ae 64 -th none -q 20 -o ${outdir}/${DATE}.jpg
